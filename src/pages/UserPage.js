38cddf5ad3255b8d9d5f0b6c82ef5d0f87d7e270
Furkan PEG <furkanp@netas.com.tr>
2020-04-07 16:55:49 +0300

    Router ekelndi. Topbar componenti eklendi.

---
+++ src/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Userpage extends Component {
+	render() {
+		return (
+			<div className="container">
+
+			</div>
+		);
+	}
+}
+
+export default Userpage;
--- src/shared/ApiProgress.js
+++ src/shared/ApiProgress.js
@@ -2,43 +2,43 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const getDisplayName = (WrappedComponent) => {
-    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+	return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
 
 export const withApiProgress = (WrappedComponent, apiPath) => {
-    return class extends Component {
+	return class extends Component {
 
-        static displayName = `ApiProgress(${getDisplayName(WrappedComponent)})`;
+		static displayName = `ApiProgress(${getDisplayName(WrappedComponent)})`;
 
-        state = {
-            pendingApiCall: false,
-        }
+		state = {
+			pendingApiCall: false,
+		}
 
-        componentDidMount = () => {
-            axios.interceptors.request.use((request) => {
-                this.updateApiCallFor(request.url, true);
-                return request;
-            });
+		componentDidMount = () => {
+			axios.interceptors.request.use((request) => {
+				this.updateApiCallFor(request.url, true);
+				return request;
+			});
 
-            axios.interceptors.response.use((response) => {
-                this.updateApiCallFor(response.config.url, false);
-                return response;
-            }, (error) => {
-                this.updateApiCallFor(error.config.url, false);
-                throw error;
-            });
-        }
+			axios.interceptors.response.use((response) => {
+				this.updateApiCallFor(response.config.url, false);
+				return response;
+			}, (error) => {
+				this.updateApiCallFor(error.config.url, false);
+				throw error;
+			});
+		}
 
-        updateApiCallFor = (url, pendingApiCall) => {
-            if (url === apiPath) {
-                this.setState({ pendingApiCall });
-            }
-        }
+		updateApiCallFor = (url, pendingApiCall) => {
+			if (url === apiPath) {
+				this.setState({ pendingApiCall });
+			}
+		}
 
-        render() {
-            const { pendingApiCall } = this.state;
+		render() {
+			const { pendingApiCall } = this.state;
 
-            return <WrappedComponent pendingApiCall={pendingApiCall} {...this.props} />
-        }
-    }
+			return <WrappedComponent pendingApiCall={pendingApiCall} {...this.props} />
+		}
+	}
 }
