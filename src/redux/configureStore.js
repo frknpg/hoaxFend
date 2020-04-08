452143e3dbb60b199e25d0d1e3ae36db80b0aead
Furkan PEG <furkanp@netas.com.tr>
2020-04-08 15:11:44 +0300

    auth bilgileri localstore 'a eklendi

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,20 +1,32 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
 
-const defaultState = {
-  isLoggedIn: false,
-  username: undefined,
-  displayName: undefined,
-  image: undefined,
-  password: undefined
-}
-
 const configureStore = () => {
 
-  return createStore(authReducer, defaultState,
+  let defaultState = {
+    isLoggedIn: false,
+    username: undefined,
+    displayName: undefined,
+    image: undefined,
+    password: undefined
+  };
+
+  const hoaxAuth = localStorage.getItem('hoax-auth');
+  if (hoaxAuth) {
+    try {
+      defaultState = JSON.parse(hoaxAuth);
+    } catch (err) { }
+  }
+
+  const store = createStore(authReducer, defaultState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
+  store.subscribe(() => {
+    localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+  });
+
+  return store;
 }
 
 export default configureStore;
